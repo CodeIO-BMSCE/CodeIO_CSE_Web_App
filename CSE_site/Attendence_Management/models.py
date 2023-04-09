cb07fdca3546263fbac8379d45ec278c973273e3
PADMANJALI1848 <84122308+PADMANJALI1848@users.noreply.github.com>
2023-04-10 01:19:52 +0530

    included changes

--- CSE_site/Attendence_Management/models.py
+++ CSE_site/Attendence_Management/models.py
@@ -8,6 +8,10 @@ class Attendance(models.Model):
     date= models.DateField(default=timezone.now)
     section = models.CharField(max_length=2 , blank=False , null=True)
     courseTitle = models.CharField(max_length=25 , blank=False , null=True)
+        
+    def __str__(self):
+        return self.date
+
     
 class FacultyCourse(models.Model):
     courseTitle = models.CharField(max_length=25)
@@ -15,14 +19,28 @@ class FacultyCourse(models.Model):
     semester = models.CharField(max_length=1)
     section = models.CharField(max_length=1)
     faculty = models.CharField(max_length=30)
+        
+    def __str__(self):
+        return self.courseTitle
+
 
 class Student(models.Model):
     name=models.CharField(max_length=50)
     usn=models.CharField(max_length=10,unique=True)
     email = models.EmailField(unique=True)
     section=models.CharField(max_length=2)
+
+    class Meta:
+        ordering = ['usn']
+        
+    def __str__(self):
+        return self.usn
+
  
 class Course(models.Model):
     className = models.CharField(max_length=2)
     courses = models.TextField()
     facultyHandles = models.TextField()
+        
+    def __str__(self):
+        return self.className
